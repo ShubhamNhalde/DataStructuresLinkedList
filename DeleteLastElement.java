66bb927f0e80337238ceb63f0fde8f0a465d40ad
Shubham <nhaldes@gmail.com>
2022-02-18 01:37:55 +0530

    Added: Delete Last Element In The Linked List

---
+++ DeleteLastElement.java
@@ -0,0 +1,81 @@
+package com.bridgelabz.linkedlist;
+
+public class DeleteLastElement<K> {
+	
+	private AllNode head;
+	private AllNode tail;
+
+	public AllNode getHead() {
+		return head;
+	}
+
+	public AllNode getTail() {
+		return tail;
+	}
+
+	public DeleteLastElement() {
+		this.head = head;
+		this.tail = tail;
+	}
+
+	public void add(AllNode newNode) {
+		if (tail == null)
+			this.tail = newNode;
+		if (head == null)
+			this.head = newNode;
+		else {
+			AllNode tempNode = this.head;
+			this.head = newNode;
+			newNode.setNext(tempNode);
+		}
+	}
+
+	public void append(AllNode newNode) {
+		if (head == null)
+			this.head = newNode;
+		if (tail == null)
+			this.tail = newNode;
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
+	public void printMyList() {
+		System.out.print("My List is : ");
+		StringBuffer myList = new StringBuffer();
+		AllNode tempNode = head;
+		while (!tempNode.equals(tail)) {
+			myList.append(tempNode.getKey());
+			myList.append("->");
+			tempNode = tempNode.getNext();
+		}
+		myList.append(tempNode.getKey());
+		System.out.println(myList);
+	}
+
+	public void insert(AllNode myNode, AllNode newNode) {
+		AllNode tempNode = myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+
+	public AllNode deleteFirst() {
+		AllNode tempNode = this.head;
+		this.head = head.getNext();
+		System.out.println("Deleted element is :" + tempNode.getKey());
+		return tempNode;
+	}
+
+	public AllNode deleteLast() {
+		AllNode tempNode = head;
+		while (!tempNode.getNext().equals(tail)) {
+			tempNode = tempNode.getNext();
+		}
+		this.tail = tempNode;
+		tempNode = tempNode.getNext();
+		System.out.println("Deleted element is :" + tempNode.getKey());
+		return tempNode;
+	}
+
+}
