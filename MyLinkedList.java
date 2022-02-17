3428e7c3f51c85db3038c42ffd95ad4503c01012
Shubham <nhaldes@gmail.com>
2022-02-18 01:23:08 +0530

    Added: Simple Linked List Of 56, 30 and 70

---
+++ MyLinkedList.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList<K>  {
+	
+	private AllNode head;
+	private AllNode tail;
+
+	public MyLinkedList(AllNode head, AllNode tail) {
+		this.head = head;
+		this.tail = tail;
+}
+}
